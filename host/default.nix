fbea3d9c67796cad715f19dbd007f233655163c8
Jose Lane <dariankline@outlook.com>
2024-09-13 14:23:04 +0800

    [host] move wave-to-fst to host/

--- host/default.nix
+++ host/default.nix
@@ -2,16 +2,43 @@
   rustPlatform,
   pkg-config,
   udev,
+  stdenv,
+  lib,
+  zlib,
+  libclang,
 }:
 rustPlatform.buildRustPackage {
   pname = "rf-tools";
   version = "0.0.1.0";
 
   src = ./.;
+
   cargoLock = {
     lockFileContents = builtins.readFile ./Cargo.lock;
   };
 
-  buildInputs = [ udev ];
+  LIBCLANG_PATH = lib.makeLibraryPath [ libclang.lib ];
+
+  preBuild = ''
+    # From: https://github.com/NixOS/nixpkgs/blob/1fab95f5190d087e66a3502481e34e15d62090aa/pkgs/applications/networking/browsers/firefox/common.nix#L247-L253
+    # Set C flags for Rust's bindgen program. Unlike ordinary C
+    # compilation, bindgen does not invoke $CC directly. Instead it
+    # uses LLVM's libclang. To make sure all necessary flags are
+    # included we need to look in a few places.
+    export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
+      $(< ${stdenv.cc}/nix-support/libc-cflags) \
+      $(< ${stdenv.cc}/nix-support/cc-cflags) \
+      $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
+      ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
+      ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
+      -I${zlib.dev}/include \
+    "
+  '';
+
+  buildInputs = [
+    udev
+    zlib.dev
+    libclang.lib
+  ];
   nativeBuildInputs = [ pkg-config ];
 }
